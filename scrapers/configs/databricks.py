54eae99d90e269b43292928e69fe69b139d398aa
williamwu277 <williamwu277@gmail.com>
2026-03-29 00:09:41 -0400

    Add databricks integration

---
+++ scrapers/configs/databricks.py
@@ -0,0 +1,14 @@
+from simple_scrape import SimpleScrapeConfig, simple_scrape
+
+
+config = SimpleScrapeConfig(
+    company_name="Databricks",
+    base_url="https://www.databricks.com/company/careers/open-positions?department=University%20Recruiting&location=USCA",
+    jobs_selector="div#jobWrap a",
+    url_selector=":scope",
+    title_selector="span:nth-child(1)",
+    location_selector="span:nth-child(2)",
+    link_augmentation="https://www.databricks.com"
+)
+
+strategy = simple_scrape
