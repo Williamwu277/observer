3d3ef25dac73a883d48d5f10e89ac1b5deccca57
williamwu277 <williamwu277@gmail.com>
2026-05-03 17:27:09 -0400

    Add pinterest integration:

---
+++ scrapers/configs/pinterest.py
@@ -0,0 +1,14 @@
+from strategies.paginating_scrape import PaginatingScrapeConfig, paginating_scrape
+
+
+config = PaginatingScrapeConfig(
+    company_name="Pinterest",
+    base_url="https://www.pinterestcareers.com/jobs/?search=&team=University&pagesize=20#results",
+    jobs_selector='div.job-listing div.card-job',
+    url_selector="a",
+    title_selector="a",
+    location_selector="li:nth-child(1)",
+    next_page_selector="a[aria-label='Next page']",
+)
+
+strategy = paginating_scrape
