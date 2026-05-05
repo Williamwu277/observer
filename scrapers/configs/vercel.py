4c038ca1d13798140cc89a165fc10d9fd6382d11
williamwu277 <williamwu277@gmail.com>
2026-05-04 21:44:03 -0400

    Add vercel integration

---
+++ scrapers/configs/vercel.py
@@ -0,0 +1,13 @@
+from strategies.simple_scrape import SimpleScrapeConfig, simple_scrape
+
+
+config = SimpleScrapeConfig(
+    company_name="Vercel",
+    base_url="https://vercel.com/careers?location=United+States&function=Engineering",
+    jobs_selector='a[href^="/careers/"]',
+    url_selector=":scope",
+    title_selector="div p:nth-child(1)",
+    link_augmentation="https://vercel.com",
+)
+
+strategy = simple_scrape
