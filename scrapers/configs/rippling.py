7a9b21fc1191948cf0ddc63e86989f23224ac38e
williamwu277 <williamwu277@gmail.com>
2026-06-28 22:52:06 -0700

    Fix Rippling scraper

--- scrapers/configs/rippling.py
+++ scrapers/configs/rippling.py
@@ -3,11 +3,11 @@ from strategies.simple_scrape import SimpleScrapeConfig, simple_scrape
 
 config = SimpleScrapeConfig(
     company_name="Rippling",
-    base_url="https://www.rippling.com/en-CA/careers/open-roles",
+    base_url="https://www.rippling.com/en-CA/careers/open-roles?search=intern&department=Engineering",
     jobs_selector="a[href^='https://ats.rippling.com/rippling/jobs/']",
     url_selector=":scope",
-    title_selector=":scope > p",
-    location_selector="div > p",
+    title_selector=":scope > div:nth-child(1) > span:nth-child(1)",
+    location_selector=":scope > div:nth-child(1) > div",
 )
 
 strategy = simple_scrape
