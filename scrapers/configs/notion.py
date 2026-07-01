ca9d43bc080a004a33a24bb219283fb02faa77b1
williamwu277 <williamwu277@gmail.com>
2026-07-01 00:45:57 -0700

    Fix notion scraper

--- scrapers/configs/notion.py
+++ scrapers/configs/notion.py
@@ -4,10 +4,10 @@ from strategies.simple_scrape import SimpleScrapeConfig, simple_scrape
 config = SimpleScrapeConfig(
     company_name="Notion",
     base_url="https://www.notion.com/careers",
-    jobs_selector="section[aria-labelledby='open-positions-university'] a",
+    jobs_selector="section[aria-labelledby='open-positions-earlycareer'] a",
     url_selector=":scope",
-    title_selector="div:nth-child(1)",
-    location_selector="div:nth-child(2)",
+    title_selector="span > span:nth-child(1)",
+    location_selector="span > span:nth-child(2)",
 )
 
 strategy = simple_scrape
