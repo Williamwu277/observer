4d555b6c13bef7a71a13fe43ee5cdfc93400bc7c
williamwu277 <williamwu277@gmail.com>
2026-08-22 16:45:34 -0400

    Update Coinbase

--- scrapers/configs/coinbase.py
+++ scrapers/configs/coinbase.py
@@ -3,12 +3,13 @@ from strategies.simple_scrape import SimpleScrapeConfig, simple_scrape
 
 config = SimpleScrapeConfig(
     company_name="coinbase",
-    base_url="https://www.coinbase.com/en-ca/careers/positions?",
+    base_url="https://www.coinbase.com/en-ca/careers/positions?department=Engineering%2CEngineering%2520-%2520Backend%2CEngineering%2520-%2520Frontend%2CEngineering%2520-%2520Infrastructure%2CEngineering%2520-%2520Managers%2CEngineering%2520-%2520Security&search=intern",
     jobs_selector='div:has(> h2:has-text("Internships")) div:has(> a[href^="/careers/positions/"])',
     url_selector="a",
     title_selector="a > span > p",
     location_selector=":scope > p",
-    link_augmentation=lambda url: "https://www.coinbase.com/en-ca" + url
+    link_augmentation=lambda url: "https://www.coinbase.com/en-ca" + url,
+    empty_portal_selector="h3:has-text('Want to see more opportunities?')"
 )
 
 strategy = simple_scrape
