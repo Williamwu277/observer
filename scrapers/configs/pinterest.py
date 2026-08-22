8f22e3a101a5818ee5371ba7062f5e8df13eb7d2
williamwu277 <williamwu277@gmail.com>
2026-08-22 16:40:13 -0400

    Update Pinterest

--- scrapers/configs/pinterest.py
+++ scrapers/configs/pinterest.py
@@ -3,13 +3,14 @@ from strategies.paginating_scrape import PaginatingScrapeConfig, paginating_scra
 
 config = PaginatingScrapeConfig(
     company_name="pinterest",
-    base_url="https://www.pinterestcareers.com/jobs/?search=&team=University&pagesize=20#results",
+    base_url="https://www.pinterestcareers.com/jobs/?search=intern&pagesize=20#results",
     jobs_selector='div.job-listing div.card-job',
     url_selector="a",
     title_selector="a",
     location_selector="li:nth-child(1)",
     next_page_selector="a[aria-label='Next page']",
-    link_augmentation=lambda url: "https://www.pinterestcareers.com" + url
+    link_augmentation=lambda url: "https://www.pinterestcareers.com" + url,
+    empty_portal_selector="form.js-job-search-form"
 )
 
 strategy = paginating_scrape
