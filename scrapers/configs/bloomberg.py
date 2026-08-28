a17694bd1b9aa213a154905b9d8b01133b0c547b
williamwu277 <williamwu277@gmail.com>
2026-08-28 18:28:49 -0400

    Update Bloomberg to support empty portal

--- scrapers/configs/bloomberg.py
+++ scrapers/configs/bloomberg.py
@@ -4,10 +4,11 @@ from strategies.simple_scrape import SimpleScrapeConfig, simple_scrape
 config = SimpleScrapeConfig(
     company_name="bloomberg",
     base_url="https://bloomberg.avature.net/careers/SearchJobs/?1686=%5B55479%5D&1686_format=2312&listFilterMode=1&jobRecordsPerPage=12&",
-    jobs_selector="article",
+    jobs_selector="article:has(h3 a)",
     url_selector="h3 a",
     title_selector="h3",
     location_selector="span",
+    empty_portal_selector="article:has-text('There are currently no open roles matching your search')"
 )
 
 strategy = simple_scrape
