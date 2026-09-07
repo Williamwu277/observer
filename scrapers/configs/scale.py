60eb06614d1c5a6a08cab82a3dfe538945749e5e
williamwu277 <williamwu277@gmail.com>
2026-09-07 13:08:40 -0400

    Add ScaleAI

---
+++ scrapers/configs/scale.py
@@ -0,0 +1,13 @@
+from strategies.simple_scrape import SimpleScrapeConfig, simple_scrape
+
+
+config = SimpleScrapeConfig(
+    company_name="scale",
+    base_url="https://scale.com/careers",
+    jobs_selector="div#department-university a",
+    url_selector=":scope",
+    title_selector="> span",
+    link_augmentation=lambda url: "https://scale.com" + url
+)
+
+strategy = simple_scrape
